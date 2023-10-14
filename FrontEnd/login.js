399a6225b833fb76ba4ec3ca11fc67361f9a118e
Brico97442 <amanvilledamien@hotmail.fr>
2023-10-14 14:44:59 +0400

    modification and add request token system add , and user connected

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,29 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userToken = localStorage.getItem("userToken");
+  const logoutBtn = document.getElementById("logout__btn");
+
+  if (userToken) {
+    updateLogoutButton();
+  } else {
+    logoutBtn.style.display = "none";
+  }
+});
+
 const form = document.getElementById("login__form");
-form.addEventListener("submit", function (event) {
-  
+
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  
-  fetch("http://localhost:5678/api/users/login", {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
       accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: email,
-      password: password,
+      email,
+      password,
     }),
-  })
-  .then((response) => {
-      if (!response.ok) {
-        throw new Error("Erreur dans l'identifiant ou le mot de passe");
-      }
-      return response.json();
-  })
-  .then(() => {
-      window.location.href = "index.html";
   });
-  
+
+  if (response.ok) {
+    const data = await response.json();
+    localStorage.setItem("userToken", JSON.stringify({ token: data.token }));
+    updateLogoutButton();
+    window.location.href = "index.html";
+  } else {
+    const messageError = document.querySelector("#error-message");
+    messageError.innerHTML = "Erreur dans l'identifiant ou le mot de passe";
+  }
 });
+
+const updateLogoutButton = () => {
+  const logoutBtn = document.getElementById("logout__btn");
+  const loginBtn = document.getElementById("login__btn");
+  
+  if (logoutBtn) {
+    logoutBtn.style.display = "block";
+    loginBtn.style.display = "none"
+  }
+};
+
+const logoutBtn = document.getElementById("logout__btn");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    // Supprimer le token utilisateur du localStorage
+    localStorage.removeItem("userToken");
+    // Rediriger vers la page de connexion (ou toute autre page appropriée)
+    window.location.href = "login.html";
+  });
+}
